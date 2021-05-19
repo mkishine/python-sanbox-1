060712c22f222c9af46859566b91986f024a35ab
Michael Kishinevsky <michael.kishinevsky@blackrock.com>
2021-05-19 16:53:11 -0400

    Reformatted with IntelliJ. Also make test run only on portrisk-vm-2

--- test/test_synapse_connection.py
+++ test/test_synapse_connection.py
@@ -1,23 +1,25 @@
 import unittest
 import pyodbc
 import keyring
+import socket
+
 
 class TestSynapseConnection(unittest.TestCase):
-
+    @unittest.skipUnless(socket.gethostname() == "portrisk-vm-2", "run this in vm only")
     def test_first_ever(self):
         # given
-        dsn='PortriskSynapse'
-        db='portrisk_synapse_workspace_sql_pool'
-        user='sqladminuser'
-        pwd=keyring.get_password("synapse", user)
+        dsn = 'PortriskSynapse'
+        user = 'sqladminuser'
+        pwd = keyring.get_password("synapse", user)
         # when
-        connStr='DSN={}; UID={}; PWD={}'.format(dsn, user, pwd)
-        conn=pyodbc.connect(connStr)
+        conn_str = 'DSN={}; UID={}; PWD={}'.format(dsn, user, pwd)
+        conn = pyodbc.connect(conn_str)
         cursor = conn.cursor()
         cursor.execute("SELECT cusip from dbo.exp where cusip='00037BAB8'")
         # then
         cusips = [row.cusip for row in cursor.fetchall()]
         self.assertListEqual(cusips, ["00037BAB8"])
+
 
 if __name__ == '__main__':
     unittest.main()
