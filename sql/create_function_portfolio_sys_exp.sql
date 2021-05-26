dbeadcfffb8411ab92dd4be98757ccee493f2147
Aakash Kharul <kharulaakash@gmail.com>
2021-05-26 09:21:54 +0000

    Updated create function for sys exposure aggregation

--- sql/create_function_portfolio_sys_exp.sql
+++ sql/create_function_portfolio_sys_exp.sql
@@ -1,24 +1,25 @@
-IF
-OBJECT_ID('dbo.portfolio_sys_exp', 'IF') IS NOT NULL
-DROP FUNCTION dbo.portfolio_sys_exp
-    GO
-
-CREATE FUNCTION dbo.portfolio_sys_exp(@portfolio_name CHAR(10), @date DATE)
-    RETURNS TABLE AS
-RETURN(
-SELECT fid, sum(p.amount * s.exp / e.notional) as exp
-FROM dbo.sys2 s,
-     dbo.exp2 e,
-     dbo.pos p
-WHERE s.s_cusip_id = e.e_cusip_id
-  AND e.cusip = p.cusip
-  AND e.dt = p.pos_date
-  AND p.portfolio_name = @portfolio_name
-  AND p.pos_date = @date
-GROUP BY fid
-)
+IF OBJECT_ID('dbo.portfolio_sys_exposures', 'IF') IS NOT NULL
+    DROP FUNCTION dbo.portfolio_sys_exposures
 GO
+ 
+CREATE FUNCTION dbo.portfolio_sys_exposures(@portfolio_name CHAR(10), @date DATE)
+    RETURNS TABLE AS RETURN(
+        SELECT sys_exposures.ftag, sum(weights.weight * sys_exposures.exp) as exposure_aggregate
+        from dbo.sys as sys_exposures, 
+            (
+                SELECT exposures.e_cusip_id as cusip_id, sum(amount)/max(total_value) as weight
+                from dbo.pos positions, (SELECT sum(amount) as total_value
+                        from dbo.pos
+                        where pos_date=@date and portfolio_name=@portfolio_name
+                    ) total, 
+                    dbo.exp exposures 
+                where exposures.cusip = positions.cusip
+                    and positions.pos_date=@date
+                    and positions.portfolio_name=@portfolio_name
+                group by exposures.e_cusip_id) as weights
+        where weights.cusip_id = sys_exposures.s_cusip_id
+        group by sys_exposures.ftag
+    )
 
--- SELECT top 10 * from dbo.portfolio_sys_exp('MK-TEST-1', '2019-09-27')
--- where fid < 10
--- GO
+GO
+SELECT * FROM dbo.portfolio_sys_exposures('MK-TEST-1', '2019/09/27')
--- sql/portfolio_exposure_aggregation.sql
+++ sql/portfolio_exposure_aggregation.sql
@@ -1,14 +1,15 @@
 SELECT sys_exposures.ftag, sum(weights.weight * sys_exposures.exp) as exposure_aggregate
-from dbo.exp exposures, 
-    dbo.sys as sys_exposures, 
+from dbo.sys as sys_exposures, 
     (
-        SELECT cusip, sum(amount)/max(total_value) as weight
-        from dbo.pos p, (SELECT sum(amount) as total_value
+        SELECT exposures.e_cusip_id as cusip_id, sum(amount)/max(total_value) as weight
+        from dbo.pos positions, (SELECT sum(amount) as total_value
                 from dbo.pos
                 where pos_date='2019/09/27' and portfolio_name='MK-TEST-1'
-            ) t2 
-        group by cusip
-    ) weights 
-where exposures.e_cusip_id = sys_exposures.s_cusip_id
-and exposures.cusip = weights.cusip
+            ) total, 
+            dbo.exp exposures 
+        where exposures.cusip = positions.cusip
+            and positions.pos_date='2019/09/27' 
+            and positions.portfolio_name='MK-TEST-1'
+        group by exposures.e_cusip_id) as weights
+where weights.cusip_id = sys_exposures.s_cusip_id
 group by sys_exposures.ftag
