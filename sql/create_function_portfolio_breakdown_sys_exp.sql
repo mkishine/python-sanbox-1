3a5f288178122e108c7bdb685140475039ec75bf
Aakash Kharul <kharulaakash@gmail.com>
2021-06-04 10:06:33 +0000

    Updated sys exposure for breakdown with brk2 table

--- sql/create_function_portfolio_breakdown_sys_exp.sql
+++ sql/create_function_portfolio_breakdown_sys_exp.sql
@@ -1,3 +1,4 @@
+
 IF
 OBJECT_ID('dbo.portfolio_breakdown_sys_exp', 'IF') IS NOT NULL
 DROP FUNCTION dbo.portfolio_breakdown_sys_exp
@@ -6,19 +7,19 @@ DROP FUNCTION dbo.portfolio_breakdown_sys_exp
 CREATE FUNCTION dbo.portfolio_breakdown_sys_exp(@portfolio_name CHAR(10), @date DATE, @breakdown_name char(10))
     RETURNS TABLE AS
 RETURN(
-SELECT distinct s.fid, b.level_1, b.level_2, b.level_3, b.level_4, sum(p.amount * s.exp / e.notional) as exp
-FROM dbo.sys2 s,
-     dbo.exp2 e,
-     dbo.pos p,
-     dbo.breakdown b
-WHERE s.s_cusip_id = e.e_cusip_id
-  AND e.cusip = p.cusip
-  AND e.dt = p.pos_date
-  AND p.cusip = b.cusip
-  AND p.portfolio_name = @portfolio_name
-  AND p.pos_date = @date
-  AND b.breakdown_name = @breakdown_name
-GROUP BY ROLLUP(s.fid, b.level_1, b.level_2, b.level_3, b.level_4)
+SELECT  s.fid, b.level_tag, sum(p.amount * s.exp / e.notional) as exp
+    FROM dbo.sys2 s,
+        dbo.exp2 e,
+        dbo.pos p,
+        dbo.brk2 b
+    WHERE s.s_cusip_id = e.e_cusip_id
+    AND e.cusip = p.cusip
+    AND e.dt = p.pos_date
+    AND p.cusip = b.cusip
+    AND p.portfolio_name = @portfolio_name
+    AND p.pos_date = @date
+    AND b.name = @breakdown_name
+    GROUP BY s.fid, b.level_tag
 )
 GO
 
